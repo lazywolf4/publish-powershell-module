5befecad1919530b76ba7c5b79d654686fee884f
lazywolf4 <berz@nexcon-it.de>
2024-06-05 11:26:11 +0000

    Update entrypoint.ps1

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,13 +1,7 @@
 #!/usr/bin/env pwsh
 $ErrorActionPreference = 'Stop'
 
-$Modules = $null
-if ([string]::IsNullOrWhiteSpace($env:INPUT_MODULEPATH)) {
-    $Modules = Get-ChildItem -Recurse -Filter '*.psd1' |
-        Select-Object -Unique -ExpandProperty Directory
-} else {
-    $Modules = @($env:INPUT_MODULEPATH)
-}
+$Modules = @($env:INPUT_MODULEPATH)
 
 $Modules | ForEach-Object {
     Write-Host "Publishing '$_' to PowerShell Gallery"
