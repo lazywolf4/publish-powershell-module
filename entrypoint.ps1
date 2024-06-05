a07da89814adb22856bd4005beb288b9631c2ad8
lazywolf4 <berz@nexcon-it.de>
2024-06-05 06:51:08 +0000

    Update entrypoint.ps1

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -12,6 +12,12 @@ if ([string]::IsNullOrWhiteSpace($env:INPUT_MODULEPATH)) {
 $Modules | ForEach-Object {
     Write-Host "Publishing '$_' to PowerShell Gallery"
 
+    if ($env:INPUT_AUTOVERSION -eq "true") {
+        $ModuleName = $_
+        $ModulePath = $env:INPUT_MODULEFULLPATH
+	    ./autoversion.ps1
+    }
+
     Register-PSRepository -Name "BagetNx" -SourceLocation "https://nuget.dev.nexcon-it.de/v3/index.json" -PublishLocation "https://nuget.dev.nexcon-it.de/api/v2/package" -InstallationPolicy "Trusted"
     Publish-Module -Path $_ -NuGetApiKey $env:INPUT_NUGETAPIKEY -Repository $env:INPUT_NUGETREPOSITORY
     Write-Host "'$_' published to PowerShell Gallery"
