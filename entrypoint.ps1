d58d6bb4d645c6ea5595e79024bb8560830a7835
lazywolf4 <berz@nexcon-it.de>
2024-06-07 13:27:05 +0000

    Update entrypoint.ps1

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -4,18 +4,46 @@ $ErrorActionPreference = 'Stop'
 Write-Host "Daten von Gitea..."
 Write-Host "$env:INPUT_CHANGEDFILES"
 
-$Modules = @($env:INPUT_MODULEPATH)
+$changedFilesString = "$env:INPUT_CHANGEDFILES"
+$changedFilesIgnoreString = "$env:INPUT_CHANGEDFILESIGNORE"
 
-$Modules | ForEach-Object {
-    Write-Host "Publishing '$_' to PowerShell Gallery"
+#changedFilesIgnore string to array
+$changedFilesIgnoreArray = $changedFilesIgnoreString.Split(',') 
 
-    if ($env:INPUT_AUTOVERSION -eq "true") {
-        $ModuleName = $_
-        $ModulePath = $env:INPUT_MODULEFULLPATH
-	& \data\autoversion.ps1
+#==Array erstellung aus JSON==
+# Extrahiere Werte zwischen den Anführungszeichen
+$changedFilesValues = $changedFilesString -split '\",\"' | ForEach-Object { $_ -replace '[\[\]"]', '' }
+# Erstelle ein Array aus den extrahierten Werten
+$changedFilesArray = @($changedFilesValues)
+
+#Init empty array for paths that should be processed, after scanning for invalid file extensions
+$modulePathArray = @()
+
+foreach ($changedFile in $changedFilesArray) {
+    #Extract Filename and extension
+    $filename = Split-Path -Path $changedFile -Leaf
+    $extension = $filename.Split(".")[1];
+
+
+    #Init var skipFile (this inidcates if the current path should be added to "process array")
+    [bool]$skipFile = $false
+    #Check if filetype is not on blacklist
+    foreach ($ignoreFiletype in $changedFilesIgnoreArray) {
+        if ($ignoreFiletype -eq $extension) {
+            [bool]$skipFile = $true
+            Break
+        }
     }
 
-    Register-PSRepository -Name "BagetNx" -SourceLocation "https://nuget.dev.nexcon-it.de/v3/index.json" -PublishLocation "https://nuget.dev.nexcon-it.de/api/v2/package" -InstallationPolicy "Trusted"
-    Publish-Module -Path $_ -NuGetApiKey $env:INPUT_NUGETAPIKEY -Repository $env:INPUT_NUGETREPOSITORY
-    Write-Host "'$_' published to PowerShell Gallery"
+    if (-Not ($skipFile)) {
+        #Get module paths like this "Modules/Xyz", without Filename etc.
+        $modulePathArray += $changedFile.Split("/")[0..1] -join "/"
+    }
 }
+
+
+#Dedup array
+$modulePathArray = $modulePathArray | select -Unique
+
+Write-Host "Pfade zum verarbeiten:"
+$modulePathArray
