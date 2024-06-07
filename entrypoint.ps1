e9722c2ad9b21a761352882f7681614b2d7a7b54
lazywolf4 <berz@nexcon-it.de>
2024-06-07 06:32:57 +0000

    Update entrypoint.ps1

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -3,3 +3,19 @@ $ErrorActionPreference = 'Stop'
 
 Write-Host "Daten von Gitea..."
 Write-Host "$env:INPUT_CHANGEDFILES"
+
+$Modules = @($env:INPUT_MODULEPATH)
+
+$Modules | ForEach-Object {
+    Write-Host "Publishing '$_' to PowerShell Gallery"
+
+    if ($env:INPUT_AUTOVERSION -eq "true") {
+        $ModuleName = $_
+        $ModulePath = $env:INPUT_MODULEFULLPATH
+	& \data\autoversion.ps1
+    }
+
+    Register-PSRepository -Name "BagetNx" -SourceLocation "https://nuget.dev.nexcon-it.de/v3/index.json" -PublishLocation "https://nuget.dev.nexcon-it.de/api/v2/package" -InstallationPolicy "Trusted"
+    Publish-Module -Path $_ -NuGetApiKey $env:INPUT_NUGETAPIKEY -Repository $env:INPUT_NUGETREPOSITORY
+    Write-Host "'$_' published to PowerShell Gallery"
+}
